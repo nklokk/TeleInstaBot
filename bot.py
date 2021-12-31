3a80ed2fb600744b7871166a1fa410f888cd3c87
NklokkStudion <nklokkstudion@mail.ru>
2021-12-31 15:02:32 +0100

    Add Telegram buttons

--- bot.py
+++ bot.py
@@ -3,16 +3,20 @@
 
 import argparse
 import instabot
-import sqlite3 as sl
 import json
+import logging
 import os
+import sqlite3 as sl
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.filters import Text
 from aiogram.utils import executor
-from tests import mocks
 from dotenv import load_dotenv
+from tests import mocks
 
+
+logging.basicConfig(level=logging.INFO)
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
@@ -30,54 +34,118 @@ bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
 
 
-def insert_or_update_user_id(user_id, username, user_followers):
-    user_followers = json.dumps(user_followers)
-    with users_db:
-        info = users_db.execute(f'SELECT * FROM users WHERE user_id = {user_id}').fetchone()
-        if info is not None:
-            users_db.execute('UPDATE users SET followers = ?, username = ? WHERE user_id = ?', (user_followers, username, user_id))
-        else:
-            users_db.execute('INSERT INTO users (user_id, username, followers) VALUES (?, ?, ?)', (user_id, username, user_followers))
+def get_username_confirmation_keyboard(username):
+    buttons = [
+        types.InlineKeyboardButton(text='Да', callback_data='confirmation_yes_' + username),
+        types.InlineKeyboardButton(text='Нет', callback_data='confirmation_no_' + username)
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttons)
+    return keyboard
 
 
-def get_followers_from_db(user_id):
-    info = users_db.execute(f'SELECT username, followers FROM users WHERE user_id = {user_id}').fetchone()
-    if info is not None:
-        return (info[0], json.loads(info[1]))
-    else:
-        return (None, None)
+def get_menu_keyboard(username):
+    buttons = [
+        types.InlineKeyboardButton(text='Подписчики', callback_data='menu_followers_' + username),
+        types.InlineKeyboardButton(text='Подписки', callback_data='menu_following_' + username),
+        types.InlineKeyboardButton(text='Завершить', callback_data='menu_exit_' + username)
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttons)
+    return keyboard
+
+
+def get_goto_menu_keyboard(username):
+    buttons = [
+        types.InlineKeyboardButton(text='Меню', callback_data='goto_menu_' + username),
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    keyboard.add(*buttons)
+    return keyboard
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    options = message.text.split()
-    if len(options) == 2:
-        user_id = message.from_user.id
-        username = options[1]
-        user_followers = insta_bot.get_user_followers(username)
-        insert_or_update_user_id(user_id, username, user_followers)
-        await message.reply(f'Супер!\nПривязал к тебе {username}!\nНа данный момент у этого аккаунта {len(user_followers)} подписчиков')
-    else:
-        await message.reply('Напиши пожалуйста "/start <никнейм в инстаграме>"')
+    await message.answer('Введите username интересующего вас instagram пользователя.')
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply('Прежде всего напиши команду "/start <никнейм в инстаграме>"')
+    await process_start_command(message)
 
 
-@dp.message_handler(commands=['followers'])
-async def process_followers_command(message: types.Message):
-    user_id = message.from_user.id
-    username, db_followers = get_followers_from_db(user_id)
-    if db_followers is None:
-        await bot.send_message(user_id, 'Напиши пожалуйста "/start <никнейм в инстаграме>"')
+@dp.message_handler()
+async def process_username(message: types.Message):
+    if len(message.text.split()) != 1:
+        await message.answer(
+            'Instagram username должен состоять из <b>одного</b> слова',
+             parse_mode=types.ParseMode.HTML
+        )
     else:
-        user_followers = insta_bot.get_user_followers(username)
-        insert_or_update_user_id(user_id, username, user_followers)
-        delta = len(user_followers) - len(db_followers) 
-        await bot.send_message(user_id, f'У аккаунта {username} подписчиков изменилось на {delta}')
+        username = message.text
+        await message.answer(
+            f'Хотите обработать аккаунт <b>{username}</b>?',
+            reply_markup=get_username_confirmation_keyboard(username),
+            parse_mode=types.ParseMode.HTML
+        )
 
+
+@dp.callback_query_handler(Text(startswith='confirmation_'))
+async def process_username_confirmation_callback(call: types.CallbackQuery):
+    answer = call.data.split('_')[1]
+    username = call.data.split('_')[2]
+    if answer == 'yes':
+        await call.message.edit_text(
+            f'Username: <b>{username}</b>',
+            reply_markup=get_menu_keyboard(username),
+            parse_mode=types.ParseMode.HTML
+        )
+    elif answer == 'no':
+        await call.message.edit_text('Введите username еще разок')
+
+
+@dp.callback_query_handler(Text(startswith='goto_'))
+async def process_username_confirmation_callback(call: types.CallbackQuery):
+    answer = call.data.split('_')[1]
+    username = call.data.split('_')[2]
+    if answer == 'menu':
+        await call.message.edit_text(
+            f'Username: <b>{username}</b>',
+            reply_markup=get_menu_keyboard(username),
+            parse_mode=types.ParseMode.HTML
+        )
+
+
+@dp.callback_query_handler(Text(startswith='menu_'))
+async def process_menu_callback(call: types.CallbackQuery):
+    answer = call.data.split('_')[1]
+    username = call.data.split('_')[2]
+    if answer == 'exit':
+        await call.message.edit_text(
+            f'Обработка аккаунта <b>{username}</b> завершена',
+            parse_mode=types.ParseMode.HTML
+        )
+    elif answer == 'followers':
+        user_followers = insta_bot.get_user_followers(username) 
+        if user_followers:
+            user_followers = [
+                insta_bot.get_username_from_user_id(user_id)
+                for user_id in user_followers[:5]
+            ]
+            await call.message.edit_text(
+                '\n'.join(user_followers),
+                reply_markup=get_goto_menu_keyboard(username)
+            )
+        else:
+            await call.message.edit_text(
+                'У этого пользователя нету подписчиков =(',
+                reply_markup=get_goto_menu_keyboard(username)
+            )
+    elif answer == 'following':
+        await call.message.edit_text(
+            'Будет реализовано позже',
+            reply_markup=get_goto_menu_keyboard(username)
+        )
 
 if __name__ == '__main__':
     global users_db, insta_bot
